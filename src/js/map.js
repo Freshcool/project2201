8c2739eba51ced909eb68a7c757a9fdddc19e4c9
Ivan Filipov <freshcool9095@gmail.com>
2022-01-17 19:55:44 +0200

    правки

--- src/js/map.js
+++ src/js/map.js
@@ -1,14 +1,13 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[map-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[map-window]'),
-  };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('visually-hidden');
-  }
-})();
+    const refs = {
+      openModalBtn: document.querySelector('[map-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[map-window]'),
+    };
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+    function toggleModal() {
+      refs.modal.classList.toggle('visually-hidden');
+    }
+  })();
