d07bfc0303f3a99bb585fd64c47c4db872d4c3d5
Voznyuk Dmitriy Leontevich <91959188+VoznyukDmitri@users.noreply.github.com>
2022-01-19 16:47:22 +0200

    ремонт map.js

--- src/js/map.js
+++ src/js/map.js
@@ -1,13 +1,12 @@
 (() => {
-
-    const refs = {
-      openModalBtn: document.querySelector('[map-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[map-window]'),
-    };
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-    function toggleModal() {
-      refs.modal.classList.toggle('visually-hidden');
-    }
-  })();
+  const refs = {
+    openMapBtn: document.querySelector('.map-open'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('.map-window'),
+  };
+  refs.openMapBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal() {
+    refs.modal.classList.toggle('visually-hidden');
+  }
+})();
